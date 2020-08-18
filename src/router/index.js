754fdce7943eca6519c7a964a0efaca36dc765e6
wangyy-0512 <69137646+wangyy-0512@users.noreply.github.com>
2020-08-18 21:52:29 +0800

    更新 index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/components/Test'
+import BorrowMoney from '@/components/BorrowMoney'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/Test',
       name: 'Test',
       component: Test
+    },
+    {
+      path: '/BorrowMoney',
+      name: 'BorrowMoney',
+      component: BorrowMoney
     }
   ]
 })
