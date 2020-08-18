2f69af0d98bcd5f319e3c5922f6ce918f2e485a4
sunrj <sunrongjing0081@163.com>
2020-08-18 22:27:28 +0800

    sunrj
    vue页面

---
+++ src/components/VentureCapital.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <h1 style="color: red">
+        这是sunrj的页面
+        I am a girl
+        so,please take good care of me
+      </h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "VentureCapital"
+    }
+</script>
+
+<style scoped>
+
+</style>
