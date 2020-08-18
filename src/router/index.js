a5c1ef5f88c6beefc7bc009593bc5c77d24113b6
lilongkai123 <69139103+lilongkai123@users.noreply.github.com>
2020-08-18 21:36:24 +0800

    Llk

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Test from '@/components/Test'
+import p2p_llk from '@/components/p2p_llk'
 
 Vue.use(Router)
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/Test',
       name: 'Test',
       component: Test
+    },
+    {
+      path: '/p2p_llk',
+      name: 'p2p_llk',
+      component: p2p_llk
     }
   ]
 })
